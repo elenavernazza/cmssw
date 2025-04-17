d0f1f814fa46835186ea3820bc891c30701e38ec
Elena Vernazza <elena.vernazza@cern.ch>
2025-04-17 11:23:02 +0200

    Modify VertexTable to store all PVs

--- HLTrigger/NGTScouting/plugins/HLTVertexTableProducer.cc
+++ HLTrigger/NGTScouting/plugins/HLTVertexTableProducer.cc
@@ -62,7 +62,6 @@ HLTVertexTableProducer::HLTVertexTableProducer(const edm::ParameterSet& params)
 
 {
   produces<nanoaod::FlatTable>("PV");
-  produces<nanoaod::FlatTable>("otherPVs");
   produces<edm::PtrVector<reco::VertexCompositePtrCandidate>>();
 }
 
@@ -80,69 +79,84 @@ void HLTVertexTableProducer::produce(edm::Event& iEvent, const edm::EventSetup& 
 
   //pf candidates collection
   auto pfcIn = iEvent.getHandle(pfc_);
-  
-  //table for main primary vertex (only 1)
-  auto pvTable = std::make_unique<nanoaod::FlatTable>(1, pvName_, true);
-  pvTable->addColumnValue<float>("ndof", (*pvsIn)[0].ndof(), "main primary vertex number of degrees of freedom", 8);
-  pvTable->addColumnValue<float>("chi2", (*pvsIn)[0].normalizedChi2(), "main primary vertex reduced chi2", 8);
-  pvTable->addColumnValue<float>("x", (*pvsIn)[0].x(), "main primary vertex x coordinate", 10);
-  pvTable->addColumnValue<float>("y", (*pvsIn)[0].y(), "main primary vertex y coordinate", 10);
-  pvTable->addColumnValue<float>("z", (*pvsIn)[0].z(), "main primary vertex z coordinate", 16);
-  pvTable->addColumnValue<float>("xError", (*pvsIn)[0].xError(), "main primary vertex error in x coordinate", 10);
-  pvTable->addColumnValue<float>("yError", (*pvsIn)[0].yError(), "main primary vertex error in y coordinate", 10);
-  pvTable->addColumnValue<float>("zError", (*pvsIn)[0].zError(), "main primary vertex error in z coordinate", 16);
-  pvTable->addColumnValue<float>("score", pvsScoreProd.get(pvsIn.id(), 0), "main primary vertex score, i.e. sum pt2 of clustered objects", 8);
-  pvTable->addColumnValue<uint8_t>("tracksSize", (*pvsIn)[0].tracksSize(), "main primary vertex number of associated tracks");
-  
-  float pv_sumpt2 = 0.0, pv_sumpx = 0.0, pv_sumpy = 0.0;
-  for (const auto& obj : *pfcIn) {
-    // skip neutrals
-    if (obj.charge() == 0) continue;
-    double dz = fabs(obj.trackRef()->dz((*pvsIn)[0].position()));
-    bool include_pfc = false;
-    if (dz < 0.2) {
-      include_pfc = true;
-      for (size_t j = 1; j < (*pvsIn).size(); j++) {
-        double newdz = fabs(obj.trackRef()->dz((*pvsIn)[j].position()));
-        if (newdz < dz) {
-          include_pfc = false;
-          break;
-        }
-      }  // this pf candidate belongs to other PV
+    
+  std::vector<float> v_ndof;
+  std::vector<float> v_chi2;
+  std::vector<float> v_x;
+  std::vector<float> v_y;
+  std::vector<float> v_z;
+  std::vector<float> v_xError;
+  std::vector<float> v_yError;
+  std::vector<float> v_zError;
+  std::vector<uint8_t> v_is_good;
+  std::vector<uint8_t> v_nTracks;
+  std::vector<float> v_pv_score;
+  std::vector<float> v_pv_sumpt2;
+  std::vector<float> v_pv_sumpx;
+  std::vector<float> v_pv_sumpy;
+
+  for (size_t i = 0; i < (*pvsIn).size(); i++) {
+
+    v_ndof.push_back((*pvsIn)[i].ndof());
+    v_chi2.push_back((*pvsIn)[i].normalizedChi2());
+    v_x.push_back((*pvsIn)[i].x());
+    v_y.push_back((*pvsIn)[i].y());
+    v_z.push_back((*pvsIn)[i].z());
+    v_xError.push_back((*pvsIn)[i].xError());
+    v_yError.push_back((*pvsIn)[i].yError());
+    v_zError.push_back((*pvsIn)[i].zError());
+    v_nTracks.push_back((*pvsIn)[i].nTracks());
+    v_is_good.push_back(goodPvCut_((*pvsIn)[i]));
+    v_pv_score.push_back(pvsScoreProd.get(pvsIn.id(), i));
+
+    float pv_sumpt2 = 0; float pv_sumpx = 0; float pv_sumpy = 0;
+    for (const auto& obj : *pfcIn) {
+      // skip neutrals
+      if (obj.charge() == 0) continue;
+      double dz = fabs(obj.trackRef()->dz((*pvsIn)[i].position()));
+      bool include_pfc = false;
+      if (dz < 0.2) {
+        include_pfc = true;
+        for (size_t j = 0; j < (*pvsIn).size() && j != i; j++) {
+          double newdz = fabs(obj.trackRef()->dz((*pvsIn)[j].position()));
+          if (newdz < dz) {
+            include_pfc = false;
+            break;
+          }
+        }  // this pf candidate belongs to other PV
+      }
+      if (include_pfc) {
+        float pfc_pt = obj.pt();
+        pv_sumpt2 += pfc_pt * pfc_pt;
+        pv_sumpx += obj.px();
+        pv_sumpy += obj.py();
+      }
     }
-    if (include_pfc) {
-      float pfc_pt = obj.pt();
-      pv_sumpt2 += pfc_pt * pfc_pt;
-      pv_sumpx += obj.px();
-      pv_sumpy += obj.py();
-    }
+
+    v_pv_sumpt2.push_back(pv_sumpt2);
+    v_pv_sumpx.push_back(pv_sumpx);
+    v_pv_sumpy.push_back(pv_sumpy);
   }
 
-  pvTable->addColumnValue<float>("sumpt2", pv_sumpt2, "sum pt2 of pf charged candidates within dz=0.2 for the main primary vertex", 10);
-  pvTable->addColumnValue<float>("sumpx", pv_sumpx, "sum px of pf charged candidates within dz=0.2 for the main primary vertex", 10);
-  pvTable->addColumnValue<float>("sumpy", pv_sumpy, "sum py of pf charged candidates within dz=0.2 for the main primary vertex", 10);
+  //table for all primary vertices
+  auto pvTable = std::make_unique<nanoaod::FlatTable>((*pvsIn).size(), pvName_, true);
+  pvTable->addColumn<float>("ndof", v_ndof, "primary vertex number of degrees of freedom", 8);
+  pvTable->addColumn<float>("chi2", v_chi2, "primary vertex reduced chi2", 8);
+  pvTable->addColumn<float>("x", v_x, "primary vertex x coordinate", 10);
+  pvTable->addColumn<float>("y", v_y, "primary vertex y coordinate", 10);
+  pvTable->addColumn<float>("z", v_z, "primary vertex z coordinate", 16);
+  pvTable->addColumn<float>("xError", v_xError, "primary vertex error in x coordinate", 10);
+  pvTable->addColumn<float>("yError", v_yError, "primary vertex error in y coordinate", 10);
+  pvTable->addColumn<float>("zError", v_zError, "primary vertex error in z coordinate", 16);
+  pvTable->addColumn<uint8_t>("isGood", v_is_good, "wheter the primary vertex passes selection: "+ goodPvCutString_ + ")");
+  pvTable->addColumn<uint8_t>("nTracks", v_nTracks, "primary vertex number of associated tracks");
+  pvTable->addColumn<float>("score", v_pv_score, "primary vertex score, i.e. sum pt2 of clustered objects", 8);
+  pvTable->addColumn<float>("sumpt2", v_pv_sumpt2, "sum pt2 of pf charged candidates within dz=0.2 for the main primary vertex", 10);
+  pvTable->addColumn<float>("sumpx", v_pv_sumpx, "sum px of pf charged candidates within dz=0.2 for the main primary vertex", 10);
+  pvTable->addColumn<float>("sumpy", v_pv_sumpy, "sum py of pf charged candidates within dz=0.2 for the main primary vertex", 10);
   
-  //number of total (and good) primary vertices
-  int goodPVs = 0;
-  for (const auto& pv : *pvsIn)
-    if (goodPvCut_(pv))
-      goodPVs++;
-  pvTable->addColumnValue<uint8_t>("npvs", pvsIn->size(), "total number of reconstructed primary vertices");
-  pvTable->addColumnValue<uint8_t>("npvsGood", goodPVs, "number of good reconstructed primary vertices (selection:" + goodPvCutString_ + ")");
-
-  //table for other primary vertices (only storing maximum three vertices after the main one)
-  auto otherPVsTable = std::make_unique<nanoaod::FlatTable>((*pvsIn).size() > 4 ? 3 : (*pvsIn).size() - 1, "Other" + pvName_, false);
-  std::vector<float> pvsz;
-  std::vector<float> pvscores;
-  for (size_t i = 1; i < (*pvsIn).size() && i < 4; i++) {
-    pvsz.push_back((*pvsIn)[i].z());
-    pvscores.push_back(pvsScoreProd.get(pvsIn.id(), i));
-  }
-  otherPVsTable->addColumn<float>("z", pvsz, "z coordinate of other primary vertices, excluding the main PV", 8);
-  otherPVsTable->addColumn<float>("score", pvscores, "scores of other primary vertices, excluding the main PV, i.e. sum pt2 of clustered objects", 8);
-
   iEvent.put(std::move(pvTable), "PV");
-  iEvent.put(std::move(otherPVsTable), "otherPVs");
+
 }
 
 // ------------ fill 'descriptions' with the allowed parameters for the module ------------
