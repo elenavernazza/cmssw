3576ae6539466448bb6927bff63b2ed4a8b9f8c9
Elena Vernazza <elena.vernazza@cern.ch>
2025-08-27 15:48:25 +0200

    Add PFCluster variables

--- Validation/RecoParticleFlow/plugins/PFTester.cc
+++ Validation/RecoParticleFlow/plugins/PFTester.cc
@@ -7,30 +7,14 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DataFormats/Common/interface/Handle.h"
-#include "DataFormats/Common/interface/RefToBase.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
-#include "DataFormats/METReco/interface/CaloMET.h"
-#include "DataFormats/METReco/interface/CaloMETCollection.h"
-#include "DataFormats/METReco/interface/GenMET.h"
-#include "DataFormats/METReco/interface/GenMETCollection.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
 #include "DataFormats/ParticleFlowReco/interface/PFBlock.h"
 #include "DataFormats/ParticleFlowReco/interface/PFBlockElement.h"
 #include "DataFormats/ParticleFlowReco/interface/PFRecTrack.h"
-#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/ParticleFlowReco/interface/PFCluster.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-
-#include <algorithm>
 #include <cmath>
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <ostream>
-#include <string>
-#include <vector>
 
 class PFTester : public DQMEDAnalyzer {
 public:
@@ -51,27 +35,36 @@ protected:
 
   MonitorElement* h_NumElements_;
   MonitorElement* h_NumTrackElements_;
+  MonitorElement* h_NumMuonElements_;
   MonitorElement* h_NumPS1Elements_;
   MonitorElement* h_NumPS2Elements_;
   MonitorElement* h_NumECALElements_;
   MonitorElement* h_NumHCALElements_;
-  MonitorElement* h_NumMuonElements_;
+  MonitorElement* h_NumHGCALElements_;
 
   MonitorElement* h_TrackCharge_;
   MonitorElement* h_TrackNumPoints_;
   MonitorElement* h_TrackNumMeasurements_;
   MonitorElement* h_TrackImpactParameter_;
 
+  MonitorElement* h_NumPFClusters_;
+  MonitorElement* h_PFClusterE_;
+  MonitorElement* h_PFClusterEta_;
+  MonitorElement* h_PFClusterPhi_;
+  MonitorElement* h_PFClusterDepth_;
+  MonitorElement* h_PFClusterNHits_;
+  MonitorElement* h_PFClusterType_;
+  MonitorElement* h_PFClusterHitFraction_;
+  MonitorElement* h_PFClusterHitDetId_;
+
 };
 
 PFTester::PFTester(const edm::ParameterSet &iConfig) 
     : PFCandToken_(consumes<reco::PFCandidateCollection>(iConfig.getParameter<edm::InputTag>("PFCand"))) {}
 
-// PFTester::~PFTester() {}
-
 void PFTester::bookHistograms(DQMStore::IBooker& ibook, edm::Run const&, edm::EventSetup const&) {
 
-  ibook.setCurrentFolder("HLT/ParticleFlow/PFValidation/PFCandidates");
+  ibook.setCurrentFolder("HLT/ParticleFlow/PFCandidates");
   h_PFCandEt_ = ibook.book1D("PFCandEt", "PFCandEt", 1000, 0, 1000);
   h_PFCandEta_ = ibook.book1D("PFCandEta", "PFCandEta", 200, -5, 5);
   h_PFCandPhi_ = ibook.book1D("PFCandPhi", "PFCandPhi", 200, -M_PI, M_PI);
@@ -79,24 +72,32 @@ void PFTester::bookHistograms(DQMStore::IBooker& ibook, edm::Run const&, edm::Ev
   h_PFCandPdgId_ = ibook.book1D("PFCandPdgId", "PFCandPdgId", 44, -22, 22);
   h_PFCandType_ = ibook.book1D("PFCandidateType", "PFCandidateType", 10, 0, 10);
 
-  ibook.setCurrentFolder("HLT/ParticleFlow/PFValidation/PFBlocks");
+  ibook.setCurrentFolder("HLT/ParticleFlow/PFBlocks");
   h_NumElements_ = ibook.book1D("NumElements", "NumElements", 25, 0, 25);
   h_NumTrackElements_ = ibook.book1D("NumTrackElements", "NumTrackElements", 5, 0, 5);
+  h_NumMuonElements_ = ibook.book1D("NumMuonElements", "NumMuonElements", 5, 0, 5);
   h_NumPS1Elements_ = ibook.book1D("NumPS1Elements", "NumPS1Elements", 5, 0, 5);
   h_NumPS2Elements_ = ibook.book1D("NumPS2Elements", "NumPS2Elements", 5, 0, 5);
   h_NumECALElements_ = ibook.book1D("NumECALElements", "NumECALElements", 5, 0, 5);
   h_NumHCALElements_ = ibook.book1D("NumHCALElements", "NumHCALElements", 5, 0, 5);
-  h_NumMuonElements_ = ibook.book1D("NumMuonElements", "NumMuonElements", 5, 0, 5);
+  h_NumHGCALElements_ = ibook.book1D("NumHGCALElements", "NumHGCALElements", 5, 0, 5);
 
-
-  ibook.setCurrentFolder("HLT/ParticleFlow/PFValidation/PFTracks");
+  ibook.setCurrentFolder("HLT/ParticleFlow/PFTracks");
   h_TrackCharge_ = ibook.book1D("TrackCharge", "TrackCharge", 5, -2, 2);
   h_TrackNumPoints_ = ibook.book1D("TrackNumPoints", "TrackNumPoints", 100, 0, 100);
   h_TrackNumMeasurements_ = ibook.book1D("TrackNumMeasurements", "TrackNumMeasurements", 100, 0, 100);
   h_TrackImpactParameter_ = ibook.book1D("TrackImpactParameter", "TrackImpactParameter", 1000, 0, 1);
 
-  ibook.setCurrentFolder("HLT/ParticleFlow/PFValidation/PFClusters");
-  // to be done
+  ibook.setCurrentFolder("HLT/ParticleFlow/PFClusters");
+  h_NumPFClusters_ = ibook.book1D("NumPFClusters", "Number of PFClusters per PFCandidate", 25, 0, 25);
+  h_PFClusterE_ = ibook.book1D("PFClusterE", "PFCluster Energy;E [GeV]", 100, 0, 100);
+  h_PFClusterEta_ = ibook.book1D("PFClusterEta", "PFCluster Eta;#eta", 120, -6, 6);
+  h_PFClusterPhi_ = ibook.book1D("PFClusterPhi", "PFCluster Phi;#phi", 128, -3.2, 3.2);
+  h_PFClusterDepth_ = ibook.book1D("PFClusterDepth", "PFCluster Depth;Depth", 10, 0, 10);
+  h_PFClusterNHits_ = ibook.book1D("PFClusterNHits", "PFCluster Number of Hits", 100, 0, 100);
+  h_PFClusterType_ = ibook.book1D("PFClusterEtaWidth", "PFCluster Eta Width;#sigma_{#eta}", 20, 0, 20);
+  h_PFClusterHitFraction_ = ibook.book1D("PFClusterHitFraction", "PFCluster Hit Fraction;Fraction", 100, 0.0, 1.1);
+  h_PFClusterHitDetId_ = ibook.book1D("PFClusterHitDetId", "PFCluster Hit DetId modulo 10000;DetId mod 10000", 100, 0, 10000);
 
 }
 
@@ -113,29 +114,26 @@ void PFTester::analyze(const edm::Event &iEvent, const edm::EventSetup &) {
   }
 
   // Loop Over Particle Flow Candidates
-  reco::PFCandidateCollection::const_iterator pf;
-  for (pf = pf_candidates->begin(); pf != pf_candidates->end(); pf++) {
-    const reco::PFCandidate *particle = &(*pf);
+  for (const auto& particle : *pf_candidates) {
 
-    h_PFCandEt_->Fill(particle->et());
-    h_PFCandEta_->Fill(particle->eta());
-    h_PFCandPhi_->Fill(particle->phi());
-    h_PFCandCharge_->Fill(particle->charge());
-    h_PFCandPdgId_->Fill(particle->pdgId());
-    h_PFCandType_->Fill(particle->particleId());
-    // particle->elementsInBlocks();
+    h_PFCandEt_->Fill(particle.et());
+    h_PFCandEta_->Fill(particle.eta());
+    h_PFCandPhi_->Fill(particle.phi());
+    h_PFCandCharge_->Fill(particle.charge());
+    h_PFCandPdgId_->Fill(particle.pdgId());
+    h_PFCandType_->Fill(particle.particleId());
 
     // Get the PFBlock and Elements
-    // reco::PFBlock block = *(particle->Blocks());
-    // edm::OwnVector<reco::PFBlockElement> elements = block.elements();
-    const reco::PFCandidate::ElementsInBlocks& elementsInBlocks = particle->elementsInBlocks();
+    const reco::PFCandidate::ElementsInBlocks& elementsInBlocks = particle.elementsInBlocks();
     int numElements = elementsInBlocks.size();
     int numTrackElements = 0;
+    int numMuonElements = 0;
     int numPS1Elements = 0;
     int numPS2Elements = 0;
     int numECALElements = 0;
     int numHCALElements = 0;
-    int numMuonElements = 0;
+    int numHGCALElements = 0;
+    int numPFClusters = 0;
 
     // Loop over Elements in Block
     for (const auto& elemBlockPair : elementsInBlocks) {
@@ -161,41 +159,51 @@ void PFTester::analyze(const edm::Event &iEvent, const edm::EventSetup &) {
           double x = point->position().x();
           double y = point->position().y();
           double z = point->position().z();
-          //switch (point_layer) {
-          //case PFTrajectoryPoint::ClosestApproach:
-          // Fill the Track's D0
           if (point_layer == reco::PFTrajectoryPoint::ClosestApproach) {
-            h_TrackImpactParameter_->Fill(sqrt(x*x + y*y + z*z));
+            h_TrackImpactParameter_->Fill(sqrt(x*x + y*y + z*z)); // [FIXME]
           }
         }
         numTrackElements++;
-      }
-
-      // Element is an ECAL Cluster
-      else if (element_type == reco::PFBlockElement::ECAL) {
-        numECALElements++;
-      }
-      // Element is a HCAL Cluster
-      else if (element_type == reco::PFBlockElement::HCAL) {
-        numHCALElements++;
-      }
-      // Element is a Muon Track
+      } 
       else if (element_type == reco::PFBlockElement::MUON) {
-        numMuonElements++;
+        numMuonElements++; // Element is a Muon Track
+      } else { 
+        if (element_type == reco::PFBlockElement::PS1) numPS1Elements++; // Element is a PreShower1 Cluster
+        if (element_type == reco::PFBlockElement::PS2) numPS2Elements++; // Element is a PreShower2 Cluster
+        if (element_type == reco::PFBlockElement::ECAL) numECALElements++; // Element is an ECAL Cluster
+        if (element_type == reco::PFBlockElement::HCAL) numHCALElements++; // Element is a HCAL Cluster
+        if (element_type == reco::PFBlockElement::HGCAL) numHGCALElements++; // Element is a HGCAL Cluster
+
+        if (element.clusterRef().isNonnull()) {
+          auto const& cluster = *(element.clusterRef());
+          numPFClusters++;
+          h_PFClusterE_->Fill(cluster.energy());
+          h_PFClusterEta_->Fill(cluster.eta());
+          h_PFClusterPhi_->Fill(cluster.phi());
+          h_PFClusterDepth_->Fill(cluster.depth());
+          h_PFClusterNHits_->Fill(cluster.recHitFractions().size());
+          h_PFClusterType_->Fill(element_type);
+          for (const auto& hitFracPair : cluster.hitsAndFractions()) {
+            DetId hitId = hitFracPair.first;
+            float fraction = hitFracPair.second;
+            h_PFClusterHitFraction_->Fill(fraction);
+            h_PFClusterHitDetId_->Fill(hitId.rawId() % 10000); // modulo for visualization
+          }
+        }
       }
-      // // Element is a PS1 Cluster
-      // else if (element_type == PFBlockElement::PS1) {
-      //   numPS1Elements++;
-      // }
-      // Fill the Respective Elements Sizes
-      h_NumElements_->Fill(numElements);
-      h_NumTrackElements_->Fill(numTrackElements);
-      h_NumPS1Elements_->Fill(numPS1Elements);
-      h_NumPS2Elements_->Fill(numPS2Elements);
-      h_NumECALElements_->Fill(numECALElements);
-      h_NumHCALElements_->Fill(numHCALElements);
-      h_NumMuonElements_->Fill(numMuonElements);
     }
+
+    // Fill the Respective Elements Sizes
+    h_NumElements_->Fill(numElements);
+    h_NumPFClusters_->Fill(numPFClusters);
+    h_NumTrackElements_->Fill(numTrackElements);
+    h_NumMuonElements_->Fill(numMuonElements);
+    h_NumPS1Elements_->Fill(numPS1Elements);
+    h_NumPS2Elements_->Fill(numPS2Elements);
+    h_NumECALElements_->Fill(numECALElements);
+    h_NumHCALElements_->Fill(numHCALElements);
+    h_NumHGCALElements_->Fill(numHGCALElements);
+
   }
 }
 
--- Validation/RecoParticleFlow/python/hltPFValidation_cfi.py
+++ Validation/RecoParticleFlow/python/hltPFValidation_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-hltJetTester = cms.EDProducer("PFTester",
+hltPFTester = cms.EDProducer("PFTester",
     PFCand = cms.InputTag("hltParticleFlowTmp"),
 )
 
-PFValSeq = cms.Sequence(hltJetTester)
+PFValSeq = cms.Sequence(hltPFTester)
