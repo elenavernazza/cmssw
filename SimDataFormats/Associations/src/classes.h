c38c14f4b058829e7fa15cc3f9c436df3ce4b369
Bruno Alves <balves16@hotmail.com>
2025-09-10 16:10:19 +0200

    Remove superfluous code in classes.h.

--- SimDataFormats/Associations/src/classes.h
+++ SimDataFormats/Associations/src/classes.h
@@ -34,26 +34,3 @@
 #include "DataFormats/Common/interface/OneToManyWithQualityGeneric.h"
 #include "DataFormats/Common/interface/AssociationMapHelpers.h"
 #include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
-
-namespace {
-  struct dictionary {
-    // The produced types
-    edm::Wrapper<edm::RefProd<std::vector<reco::PFCluster>>> w_refPF;
-    edm::Wrapper<edm::RefProd<std::vector<SimCluster>>> w_refSim;
-
-    edm::Wrapper<edm::helpers::KeyVal<edm::RefProd<std::vector<SimCluster>>, edm::RefProd<std::vector<reco::PFCluster>>>> w_keyVal1;
-    edm::Wrapper<edm::helpers::KeyVal<edm::RefProd<std::vector<reco::PFCluster>>, edm::RefProd<std::vector<SimCluster>>>> w_keyVal2;
-
-    edm::Wrapper<edm::AssociationMap<edm::OneToManyWithQualityGeneric<
-      std::vector<SimCluster>, std::vector<reco::PFCluster>, std::pair<float,float>, unsigned int,
-      edm::RefProd<std::vector<SimCluster>>, edm::RefProd<std::vector<reco::PFCluster>>,
-      edm::Ref<std::vector<SimCluster>>, edm::Ref<std::vector<reco::PFCluster>>
-    >>> w_assoc1;
-
-    edm::Wrapper<edm::AssociationMap<edm::OneToManyWithQualityGeneric<
-      std::vector<reco::PFCluster>, std::vector<SimCluster>, float, unsigned int,
-      edm::RefProd<std::vector<reco::PFCluster>>, edm::RefProd<std::vector<SimCluster>>,
-      edm::Ref<std::vector<reco::PFCluster>>, edm::Ref<std::vector<SimCluster>>
-    >>> w_assoc2;
-  };
-}
