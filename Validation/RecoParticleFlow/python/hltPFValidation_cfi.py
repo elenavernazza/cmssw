3074192ca66081ed0269b10835abb356d1d44078
Bruno Alves <balves16@hotmail.com>
2025-10-16 10:55:10 +0200

    Define ECAL settings.

--- Validation/RecoParticleFlow/python/hltPFValidation_cfi.py
+++ Validation/RecoParticleFlow/python/hltPFValidation_cfi.py
@@ -1,70 +1,41 @@
 import FWCore.ParameterSet.Config as cms
 
-# lcAssocByEnergyScoreProducer = _lcAssocByEnergyScoreProducer.clone(hardScatterOnly = cms.bool(True))
-# scAssocByEnergyScoreProducer = _scAssocByEnergyScoreProducer.clone(hardScatterOnly = cms.bool(True))
-
-# layerClusterCaloParticleAssociation = cms.EDProducer("LCToCPAssociatorEDProducer",
-#     associator = cms.InputTag('lcAssocByEnergyScoreProducer'),
-#     label_cp = cms.InputTag("mix","MergedCaloTruth"),
-#     label_lc = cms.InputTag("hgcalMergeLayerClusters")
-# )
-
-# hltLcAssocByEnergyScoreProducer = _lcAssocByEnergyScoreProducer.clone(
-#     hits = cms.VInputTag("hltHGCalRecHit:HGCEERecHits", "hltHGCalRecHit:HGCHEFRecHits", "hltHGCalRecHit:HGCHEBRecHits"),
-#     hitMapTag = cms.InputTag("hltRecHitMapProducer","hgcalRecHitMap"),
-# )
-
-# hltScAssocByEnergyScoreProducer = _scAssocByEnergyScoreProducer.clone(
-#     hits = cms.VInputTag("hltHGCalRecHit:HGCEERecHits", "hltHGCalRecHit:HGCHEFRecHits", "hltHGCalRecHit:HGCHEBRecHits"),
-#     hitMapTag = cms.InputTag("hltRecHitMapProducer","hgcalRecHitMap"),
-# )
-
-# hltLayerClusterCaloParticleAssociationProducer = layerClusterCaloParticleAssociation.clone(
-#     associator = cms.InputTag("hltLcAssocByEnergyScoreProducer"),
-#     label_lc = cms.InputTag("hltMergeLayerClusters")
-# )
-
-# hltLayerClusterSimClusterAssociationProducer = _layerClusterSimClusterAssociationProducer.clone(
-#     associator = cms.InputTag("hltScAssocByEnergyScoreProducer"),
-#     label_lcl = cms.InputTag("hltMergeLayerClusters")
-# )
-
-hltPFHBHEScAssocByEnergyScoreProducer = cms.EDProducer("BarrelPCToSCAssociatorByEnergyScoreProducer",
+hltPFScAssocByEnergyScoreProducer = cms.EDProducer("BarrelPCToSCAssociatorByEnergyScoreProducer",
     hardScatterOnly = cms.bool(True),
     hitMapTag = cms.InputTag("hltRecHitMapProducer:barrelRecHitMap"),
     hits = cms.VInputTag("hltParticleFlowRecHitECALUnseeded", "hltParticleFlowRecHitHBHE"), # hltParticleFlowClusterHO
 )
 
-hltPFClusterSimClusterAssociationProducer = cms.EDProducer("PCToSCAssociatorEDProducer",
-    associator = cms.InputTag("hltPFHBHEScAssocByEnergyScoreProducer"),
-    label_lcl = cms.InputTag("hltParticleFlowClusterHBHE"),
+hltPFClusterSimClusterAssociationProducerECAL = cms.EDProducer("PCToSCAssociatorEDProducer",
+    associator = cms.InputTag("hltPFScAssocByEnergyScoreProducer"),
+    label_lcl = cms.InputTag("hltParticleFlowClusterECALUnseeded"),
     label_scl = cms.InputTag("mix","MergedCaloTruth")
 )
 
-hltPFHBHECpAssocByEnergyScoreProducer = cms.EDProducer("BarrelPCToCPAssociatorByEnergyScoreProducer",
+hltPFCpAssocByEnergyScoreProducer = cms.EDProducer("BarrelPCToCPAssociatorByEnergyScoreProducer",
     hardScatterOnly = cms.bool(True),
     hitMapTag = cms.InputTag("hltRecHitMapProducer:barrelRecHitMap"),
     hits = cms.VInputTag("hltParticleFlowRecHitECALUnseeded", "hltParticleFlowRecHitHBHE"), # hltParticleFlowClusterHO
 )
 
-hltPFClusterCaloParticleAssociationProducer = cms.EDProducer("PCToCPAssociatorEDProducer",
-    associator = cms.InputTag("hltPFHBHECpAssocByEnergyScoreProducer"),
-    label_lc = cms.InputTag("hltParticleFlowClusterHBHE"),
+hltPFClusterCaloParticleAssociationProducerECAL = cms.EDProducer("PCToCPAssociatorEDProducer",
+    associator = cms.InputTag("hltPFCpAssocByEnergyScoreProducer"),
+    label_lc = cms.InputTag("hltParticleFlowClusterECALUnseeded"),
     label_cp = cms.InputTag("mix","MergedCaloTruth")
 )
 
-hltPFTester = cms.EDProducer("PFTester",
+hltPFTesterECAL = cms.EDProducer("PFTester",
     PFCand = cms.InputTag("hltParticleFlowTmp"),
-    PFClusterHCAL = cms.InputTag("hltParticleFlowClusterHBHE"),
+    PFClusterHCAL = cms.InputTag("hltParticleFlowClusterECALUnseeded"),
     SimClusterHCAL = cms.InputTag("mix","MergedCaloTruth"),
-    PFClusterSimClusterAssociatorHCAL = cms.InputTag("hltPFClusterSimClusterAssociationProducer"),
-    PFClusterCaloParticleAssociatorHCAL = cms.InputTag("hltPFClusterCaloParticleAssociationProducer"),
+    PFClusterSimClusterAssociatorHCAL = cms.InputTag("hltPFClusterSimClusterAssociationProducerECAL"),
+    PFClusterCaloParticleAssociatorHCAL = cms.InputTag("hltPFClusterCaloParticleAssociationProducerECAL"),
 )
 
 PFValSeq = cms.Sequence(
-    hltPFHBHEScAssocByEnergyScoreProducer
-    +hltPFClusterSimClusterAssociationProducer
-    +hltPFHBHECpAssocByEnergyScoreProducer
-    +hltPFClusterCaloParticleAssociationProducer
-    +hltPFTester
+    hltPFScAssocByEnergyScoreProducer
+    +hltPFClusterSimClusterAssociationProducerECAL
+    +hltPFCpAssocByEnergyScoreProducer
+    +hltPFClusterCaloParticleAssociationProducerECAL
+    +hltPFTesterECAL
 )
