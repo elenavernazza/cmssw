e51b203cbfce653d8da0f8b99261f41c58cd9447
Elena Vernazza <elena.vernazza@cern.ch>
2026-01-19 12:16:36 +0100

    Make configuration compatible with ticl_v4

--- Validation/RecoTau/python/ticlTauValidation_cfi.py
+++ Validation/RecoTau/python/ticlTauValidation_cfi.py
@@ -14,11 +14,19 @@ hltTiclTauValidator = _ticlTauValidator.clone(
     pf          = cms.InputTag("hltParticleFlowTmp"),
     pfTmpBarrel = cms.InputTag("hltParticleFlowTmpBarrel"),
     jets        = cms.InputTag("hltAK4PFJets"),
-    ticlCandidates = cms.InputTag("hltTiclCandidate"),
+    ticlCandidates = cms.InputTag("hltTiclTrackstersMerge"),
     simTracksters  = cms.InputTag("hltTiclSimTracksters","fromCPs"),
     allTrackstersToSimTrackstersAssociationsByLCs =
         cms.InputTag("hltAllTrackstersToSimTrackstersAssociationsByLCs",
-                        "hltTiclSimTrackstersfromCPsTohltTiclCandidate"),
+                        "hltTiclSimTrackstersfromCPsTohltTiclTrackstersMerge"),
     genParticles = cms.InputTag("genParticles"),
     maxAssocScore = 0.6,
+)
+
+from Configuration.ProcessModifiers.ticl_v5_cff import ticl_v5
+ticl_v5.toModify(recoTiclTauValidator,
+    ticlCandidates = cms.InputTag("hltTiclCandidate"),
+    allTrackstersToSimTrackstersAssociationsByLCs =
+        cms.InputTag("hltAllTrackstersToSimTrackstersAssociationsByLCs",
+                        "hltTiclSimTrackstersfromCPsTohltTiclCandidate"),
 )
